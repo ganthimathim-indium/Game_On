7c16bbba35118690c2f59b6a67614371dc437b4f
ganthimathi <ganthimathi.m@indiumsoft.com>
2022-01-17 18:06:36 +0530

    project setup added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+const app = express();
+
+// It parses incoming requests with JSON payloads
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+// Handling Errors
+app.use((err, req, res, next) => {
+  // console.log(err);
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+  res.status(err.statusCode).json({
+    message: err.message,
+  });
+});
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
