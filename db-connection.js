38ecbfd148163c84b3a207fbe7da5805a8d87394
ganthimathi <ganthimathi.m@indiumsoft.com>
2022-01-17 19:40:06 +0530

    connection changes added

--- db-connection.js
+++ db-connection.js
@@ -1,11 +1,40 @@
-const Pool = require('pg').Pool
+const { Pool, Client } = require("pg");
 
-const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'password',
+const credentials = {
+  user: "postgres",
+  host: "localhost",
+  database: "gameon_db_dev",
+  password: "password",
   port: 5432,
-})
+};
 
-module.exports = pool;
+// Connect with a connection pool.
+
+async function poolDemo() {
+  const pool = new Pool(credentials);
+  const now = await pool.query("SELECT NOW()");
+  await pool.end();
+
+  return now;
+}
+
+// Connect with a client.
+
+async function clientDemo() {
+  const client = new Client(credentials);
+  await client.connect();
+  const now = await client.query("SELECT NOW()");
+  await client.end();
+
+  return now;
+}
+
+// Use a self-calling function so we can use async / await.
+
+(async () => {
+  const poolResult = await poolDemo();
+  console.log("Time with pool: " + poolResult.rows[0]["now"]);
+
+  const clientResult = await clientDemo();
+  console.log("Time with client: " + clientResult.rows[0]["now"]);
+})();
