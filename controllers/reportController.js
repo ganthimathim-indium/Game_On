f100d3c8d6565a0e8c703b4e40e63a598d1376b3
vinayhnindium <92292411+vinayhnindium@users.noreply.github.com>
2022-03-21 17:13:34 +0530

    Update reportController.js

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
 /* eslint-disable camelcase */
@@ -6,34 +7,48 @@ import conn from '../db-connection.js';
 
 // Our report logic starts here
 const reportInfo = async (req, res) => {
-  // Get user input
-  const {
-    device_id, user_id, device_name, android_version, start_time, end_time, version_name, app_name, record_duration,
-  } = req.body;
+  const requestdRole = res.apiuser.user_role;
+  const authorisedRoles = ['user', 'admin', 'super admin'];
+  // const x = conn.pool.query('SELECT * FROM roles WHERE level=1 ', (errr, result) => result);
+  // console.log(x);
+  if (authorisedRoles.includes(requestdRole)) {
+    // Get user input
+    const {
+      device_id, device_name, android_version, version_name, app_name,
+    } = req.body;
 
-  // Validate user input
-  if (!(device_id && user_id)) {
-    res.status(400).send('DeviceId and UserId is required');
-  }
-
-  const created_on = new Date();
-  conn.pool.query('INSERT INTO report_basicinfo (device_id, user_id, device_name, android_version, start_time, end_time, version_name, app_name, record_duration,created_at) VALUES ($1, $2, $3, $4,$5,$6, $7, $8, $9,$10)', 
-                  [device_id, user_id, device_name, android_version, created_on, created_on, version_name, app_name, record_duration, created_on],
-                  async (error, results) => {
-    if (error) {
-       res.status(404).json({
-        message: 'Report Not inserted....',
-        status: 'false',
-      });
+    // Validate user input
+    if (!device_id) {
+      res.status(400).send('DeviceId is required');
     }
-    const result = [results.rows[0]];
-    req.session.device_id = req.body.device_id;
     req.session.sessionID = Math.random() * 8;
-    res.status(200).json({
-      message: 'Report has been Added Successfully....',
-      status: 'true',
-      data: result,
+    req.session.sessionUserID = res.apiuser.user_id;
+    req.session.userRole = requestdRole;
+    // remove below line
+    console.log(req.session.sessionID);
+    const created_on = new Date();
+    conn.pool.query(
+      'INSERT INTO report_basicinfo (device_id, user_id, device_name, android_version, version_name, app_name,created_at,session_id) VALUES ($1, $2, $3, $4,$5,$6, $7, $8)',
+      [device_id, res.apiuser.user_id, device_name, android_version, version_name, app_name, created_on, req.session.sessionID],
+      async (error) => {
+        if (error) {
+          res.status(404).json({
+            message: error,
+            status: 'false',
+          });
+        }
+        req.session.device_id = req.body.device_id;
+        res.status(200).json({
+          message: 'Report has been Added Successfully....',
+          status: 'true',
+        });
+      },
+    );
+  } else {
+    res.status(401).json({
+      status: false,
+      message: 'Sorry, User role is not authorised.',
     });
-  });
+  }
 };
 export default reportInfo;
