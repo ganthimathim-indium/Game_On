7b2c33cd9efdd29966258c079ec04f2dacdec013
vinayhnindium <vinay.hatyal@indiumsoft.com>
2022-03-23 19:02:44 +0530

    version_name commit

--- controllers/viewHistorySessionsController.js
+++ controllers/viewHistorySessionsController.js
@@ -13,7 +13,7 @@ const getHistory = async (req, res) => {
     const {
       fromDate, toDate,
     } = req.query;
-    const sql = 'SELECT DISTINCT UD.device_id,device_name,android_version,UD.session_id,app_name,CU.average_value as cpu_average_value, GU.average_value as gpu_average_value,MU.average_value as memmory_average_value,PU.average_value as power_average_value,RU.name as user_name,email  FROM register RU FULL JOIN report_basicinfo UD ON UD.user_id = RU.id  FULL JOIN cpu_report CU ON  UD.session_id = CU.session_id FULL JOIN gpu_usage_report GU ON  CU.session_id = GU.session_id FULL JOIN memory_report MU ON  GU.session_id = MU.session_id FULL JOIN power_usage_report PU ON  MU.session_id = PU.session_id WHERE UD.session_id IN (select session_id FROM public.test_sessions WHERE created_at::date BETWEEN $1 AND $2 AND session_user_id = $3)';
+    const sql = 'SELECT DISTINCT UD.device_id,device_name,android_version,version_name,UD.session_id,app_name,CU.average_value as cpu_average_value, GU.average_value as gpu_average_value,MU.average_value as memmory_average_value,PU.average_value as power_average_value,RU.name as user_name,email  FROM register RU FULL JOIN report_basicinfo UD ON UD.user_id = RU.id  FULL JOIN cpu_report CU ON  UD.session_id = CU.session_id FULL JOIN gpu_usage_report GU ON  CU.session_id = GU.session_id FULL JOIN memory_report MU ON  GU.session_id = MU.session_id FULL JOIN power_usage_report PU ON  MU.session_id = PU.session_id WHERE UD.session_id IN (select session_id FROM public.test_sessions WHERE created_at::date BETWEEN $1 AND $2 AND session_user_id = $3)';
     conn.pool.query(sql, [fromDate, toDate, userID], (error, results) => {
       if (error) {
         res.status(500).json({
